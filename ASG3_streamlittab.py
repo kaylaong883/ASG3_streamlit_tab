164a80c53f22be82f4695a24950a753652587ecf
kaylaong883 <141047501+kaylaong883@users.noreply.github.com>
2023-08-16 21:05:34 +0800

    Update ASG3_streamlittab.py

--- ASG3_streamlittab.py
+++ ASG3_streamlittab.py
@@ -8,9 +8,9 @@ from urllib.error import URLError
 st.set_page_config(page_title='INVEMP Tasty Bytes Group 5', page_icon='🍖🍕🍜')
 
 st.sidebar.title("INVEMP: Inventory/Warehouse Management & Prediction on Sales per Menu Item")
-st.sidebar.markdown("This web app allows you to explore the internal inventory of Tasty Bytes. You can explore these functions in the web app (Description of Page)")
+st.sidebar.markdown("This web application allows you to optimize your menu and inspect customer behavior to achieve the high level goal of increasing sales 🤑💰")
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Prediction A', 'Prediction B', 'Prediction C', 'Prediction D', 'Prediction E'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Churn Prediction', 'Customer Revenue', 'Bundled Items Analysis', 'Truck Implementation', 'Sales Prediction Australia'])
 
 with tab1:
   #Tab 1 code here
